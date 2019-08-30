e5c637a7499e9f77c3c8e83eef88db97668e2700
Bruno Valle <brunolv6@gmail.com>
2019-08-30 11:53:40 -0300

    Updates

---
+++ precache-manifest.2ff80957c6d12eafde8c00235ac0f208.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e9c55a45de7da2188a223da89bc234",
+    "url": "/project-job-search/index.html"
+  },
+  {
+    "revision": "6ded3b6c31b736b4de4b",
+    "url": "/project-job-search/static/css/main.a7d3e038.chunk.css"
+  },
+  {
+    "revision": "29f24b4c9109d73dac4c",
+    "url": "/project-job-search/static/js/2.5cf87176.chunk.js"
+  },
+  {
+    "revision": "6ded3b6c31b736b4de4b",
+    "url": "/project-job-search/static/js/main.0ef1d866.chunk.js"
+  },
+  {
+    "revision": "9188053d820c5a3a5297",
+    "url": "/project-job-search/static/js/runtime~main.50f0c97f.js"
+  }
+]);
